5ef9cb8e0b805e548689388c86197395411dc8d5
wql4612 <16604735985@163.com>
2025-04-23 21:43:45 +0800

    change assignment8

---
+++ vsAssignment8/OrderManagementSystem/Context.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class Context : DbContext
+    {
+        public Context() : base("OrderDataBase")
+        {
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Goods { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ vsAssignment8/OrderSystemForm/FormDetailEdit.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OrderManagementSystem;
+
+namespace OrderSystemForm
+{
+    public partial class FormDetailEdit : Form
+    {
+        public OrderDetail Detail { get; set; }
+
+        public FormDetailEdit()
+        {
+            InitializeComponent();
+        }
+
+        public FormDetailEdit(OrderDetail detail) : this()
+        {
+            this.Detail = detail;
+            this.bdsDetail.DataSource = detail;
+            using (Context ctx = new Context())
+            {
+                bdsGoods.DataSource = ctx.Goods.ToList();
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormDetailEdit_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void cbxGoods_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
